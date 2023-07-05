29ad43e0be88501f361d943abb366e804d9e1a5d
Mohammed Imran <mohdimran97057@gmail.com>
2023-07-05 21:55:01 +0530

    Redux setUp done

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,44 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import { useGetProductsQuery } from "../slices/productsApiSlices";
+import Loader from "../components/loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState();
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  // const [products, setProducts] = useState();
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      console.log(data);
-      setProducts(data);
-    };
-
-    fetchProducts();
+    // const fetchProducts = async () => {
+    //   const { data } = await axios.get("/api/products");
+    //   console.log(data);
+    //   setProducts(data);
+    // };
+    // fetchProducts();
   }, []);
   return (
     <>
-      <h1>Latest Product</h1>
-      <Row>
-        {products?.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}{" "}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Product</h1>
+          <Row>
+            {products?.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
